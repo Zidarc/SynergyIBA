b793dbcdfb42d5c8c1f019e59a99941e781b9bd8
Ali Hussain <ali.hussaino71205o@gmail.com>
2025-01-15 20:21:40 +0500

    Added env, gitignore, mongodb connection

---
+++ functions/read.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const UserData = require("../models/userdata");
+require('dotenv').config();
+
+exports.handler = async (event, context) => {
+    try {
+        mongoose.connect(MONGODB_URI);
+        // Extract team key from the query parameters
+        const teamkey = event.queryStringParameters && event.queryStringParameters.teamkey;
+
+        if (!teamkey) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ error: "Team key is missing in the request parameters" }),
+            };
+        }
+
+        // Find data for the specified team
+        const data = await UserData.findOne({ Team_password: teamkey });
+
+        if (!data) {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ error: `Team '${teamkey}' not found` }),
+            };
+        }
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Internal Server Error" }),
+        };
+    }
+};
---
+++ models/userdata.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const coinNames = ['Bitcoin', 'Polkadot', 'Luna', 'Dogecoin', 'XRP', 'BNB','Ethereum'];
+
+const usersdata = new mongoose.Schema({
+    Team_name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    Team_password: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    free_money: {
+        type:Number,
+        required: true,
+        trim: true
+    },
+    total_worth: {
+        type:Number,
+        required: true,
+        trim: true
+    },
+    stock: {
+        type: [Number],
+        required: true,
+        validate: {
+            validator: function (v) {
+                // Ensure the length of the array matches the number of coins
+                return v.length === coinNames.length;
+            },
+            message: props => `${props.path} array length should match the number of coins`,
+        }
+    },
+    stock_previous: {
+        type: [Number],
+        required: true,
+        validate:{
+            validator: function (v) {
+                return v.lenght === coinNames.lenght;
+            },
+            message: props => `$props{prop.path} array should match the number of coins`,
+        }
+    }
+});
+
+const UserData = mongoose.model("userdata", usersdata);
+module.exports = UserData;
