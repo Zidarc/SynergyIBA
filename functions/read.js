2e88ccbb7699af863da5f195a49b9c72876914a6
Ali Hussain <ali.hussaino71205o@gmail.com>
2025-01-16 01:59:46 +0500

    check

--- functions/read.js
+++ functions/read.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { createClient } = require('@supabase/supabase-js');
+const { createClient } = require('@supabase/supabase-js'); 
 
 // Initialize Supabase client
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
