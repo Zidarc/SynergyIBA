7463830d9d42269dd0e84365a0149bc9ded36791
Ali Hussain <ali.hussaino71205o@gmail.com>
2025-01-15 20:46:16 +0500

    added login, netlify hosting

---
+++ login.js
@@ -0,0 +1,46 @@
+import { setTeamkey } from "./teamdata.js";
+function setTeamIdAfterLogin(key) {
+    setTeamkey(key);
+}
+
+window.addEventListener('pageshow', function(event) {
+    if (event.persisted || (window.performance && window.performance.navigation.type === 2)) {
+        setTeamIdAfterLogin("");
+        document.getElementById('teamName').value = "";
+        document.getElementById('password').value = "";
+    }
+});
+async function signIn() {
+    const Uteamname = document.getElementById('teamName').value;
+    const Upassword = document.getElementById('password').value;
+    setTeamIdAfterLogin(Upassword);
+    if (!Uteamname || !Upassword) {
+        document.getElementById('errorbox').innerText = "Please enter the values in.";
+    } else {
+        try {
+            const response = await fetch(`https://synergy-iba.netlify.app/.netlify/functions/read`);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            if (data.error) {
+                document.getElementById("status").innerText = `Error: ${data.error}`;
+            } else {
+                const Spassword = data.Team_password;
+
+                if (Spassword === Upassword) {
+                    // Redirect to index.js or another page on the server side
+                    window.location.href = '/user.html';
+                } else {
+                    document.getElementById('errorbox').innerText = "Password Wrong";
+                }
+            }
+        } catch (error) {
+            document.getElementById('errorbox').innerText = "An error occurred during sign-in.";
+        }
+    }
+}
+
+window.signIn = signIn;
